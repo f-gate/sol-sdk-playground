d02b6d2075c884dd6b8affe65d3277bfc0cd962d
gatsey <felixgate13@gmail.com>
2022-12-21 20:25:24 +0000

    to_sol

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,8 @@ use solana_sdk::{
     pubkey::Pubkey
 };
 
+use common::to_sol;
+
 fn main() {
     // Socket will be localhost
     let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
@@ -28,15 +30,15 @@ fn main() {
     // check if client has connected properly?
 
     // Instantiate the Accountdata page with the specified key.
-    let key = Pubkey::new("CEzN7mqP9xoxn2HdyW6fjEJ73t7qaX9Rp2zyS6hb3iEu".as_bytes());
+    let key = Pubkey::new("Aw1MMFXELAmUKvMxmvJ3RnCcQgJW7T5zM1w2f89QHdsT".as_bytes());
     if let Ok(page_data) = AccountSummeryPage::new(&client, &key) {
 
         // A connection has been made, the legitimacy of the key has not been checked.
         // If for whatever reason the key is invalid, a balance of 0 will show.
-        let mut balance: u64 = Default::default();
+        let mut balance: f64 = Default::default();
 
         if let Some(account) = page_data.account_data {
-            balance = account.lamports;  
+            balance = to_sol(account.lamports);  
         }
 
         println!("{}", balance);
